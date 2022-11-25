89dabce19fb6561312bac07ba5187e6d6244e751
German <vyrodov.german@gmail.com>
2022-11-25 22:36:28 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("German Vyrodov");
+        System.out.println("23.02.1988");
+    }
+}
